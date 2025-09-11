d7940b23822523bdd808a7b87714fc03706a86fa
Kauancode12 <kakabranco39@gmail.com>
2025-09-11 17:02:45 +0000

    Funções inserir e listar criadas e modificando

--- FreeFire.c
+++ FreeFire.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM 10
+
 typedef struct {
     char nome[30];
     char tipo[20];
@@ -9,7 +11,43 @@ typedef struct {
 } Item;
 
 int main() {
-    int mochila[10];
-    int contador_item;
+    Item mochila[TAM];
+    int contador_item = 0;
+    int opcao;
 }
+
+void inserirItem(Item mochila[], int *contador) {
+    if (*contador >= TAM) {
+        printf("Inventario cheio.\n");
+        return;
+    }
+
+    Item novo;
+    printf("Digite o nome do item: ");
+    scanf(" %[^\n]", novo.nome);
+    printf("Digite o tipo do item: ");
+    scanf(" %[^\n]", novo.tipo);
+    printf("Digite a quantidade: ");
+    scanf("%d", &novo.quantidade);
+
+    mochila[*contador] = novo;
+    (*contador)++;
+
+    printf("Item inserido com sucesso.\n");
+}
+
+void listarItens(Item mochila[], int contador) {
+    if (contador == 0) {
+        printf("Inventario vazio.\n");
+        return;
+    }
+
+    printf("=== Itens na mochila ===\n");
+    for (int i = 0; i < contador; i++)
+    {
+        printf("%d - Nome: %s | Tipo: %s | Quantidade: %d\n", i + 1, mochila[i].nome, mochila[i].tipo, mochila[i].quantidade);
+    }
+}
+
+
 
